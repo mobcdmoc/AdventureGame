5b79b6d4685e5e1f01f80f05b37cf4dcb382bccc
mobcdmoc <mobcdmoc@comcast.net>
2015-02-17 13:42:59 -0700

    Create Consts.java

---
+++ Consts.java
@@ -0,0 +1,15 @@
+package cs314.a2;
+
+public final class Consts{
+	
+	public static final byte NORTH = 0;
+	public static final byte SOUTH = 1;
+	public static final byte EAST = 2;
+	public static final byte WEST = 3;
+	public static final byte UP = 4;
+	public static final byte DOWN = 5;
+	
+	private Consts(){
+		    throw new AssertionError();
+		  }
+}
